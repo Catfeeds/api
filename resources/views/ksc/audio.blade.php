3accb219e3a72f2f2d63f41f87feeac9d1c4af1f
zhaoyang <602155776@qq.com>
2017-05-11 14:00:08 +0800

    科思创audio上传接口

--- resources/views/ksc/audio.blade.php
+++ resources/views/ksc/audio.blade.php
@@ -1,30 +1,43 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title>科思创</title>
-		<meta name="description" content="科思创">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1, maximum-scale=1,,user-scalable=no">
-		<meta name="format-detection" content="telephone=no,email=no" />
-		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
-		<link rel="stylesheet" href="{{asset('css/common.css')}}" />
-		<link rel="stylesheet" href="{{asset('css/index.css')}}">
-	</head>
-	<body>
-		<div class="page position">
-			<audio id="audio" preload>
-				<source src="{{asset($path)}}"></source>
-			</audio>
-			<div class="text position">
-				<img src="{{asset('img/text.png')}}"/>
-			</div>
-			<div class="center position">
-				<img src="{{asset('img/4_03.png')}}"/>
-			</div>
-			<div class="btm position">
-				<img src="{{asset('img/2_03.png')}}"/>
-			</div>
-		</div>
-		
-	</body>
+<head>
+    <meta charset="utf-8"/>
+    <title>科思创</title>
+    <meta name="description" content="科思创">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0, minimum-scale=1, maximum-scale=1,,user-scalable=no">
+    <meta name="format-detection" content="telephone=no,email=no"/>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1"/>
+    <link rel="stylesheet" href="{{asset('css/common.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+</head>
+<body>
+<div class="page position">
+    <audio id="audio" preload>
+        <source src="{{asset($path)}}"></source>
+    </audio>
+    <div class="text position">
+        <img src="{{asset('img/text.png')}}"/>
+    </div>
+    <div class="center position">
+        <img src="{{asset('img/4_03.png')}}"/>
+    </div>
+    <div class="btm position">
+        <img id="playBtm" src="{{asset('img/2_03.png')}}"/>
+    </div>
+</div>
+
+</body>
+<script type="text/javascript">
+    var playBtm = document.getElementById('playBtm');
+    var audio = document.getElementById('audio')
+
+    playBtm.addEventListener('touchstart',function(){
+
+        audio.play();
+
+    })
+
+
+</script>
 </html>
