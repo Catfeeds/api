6f2083e9bef1ed7b72476cf66fd9286f65ef9d85
zhougogo <1290612478@qq.com>
2017-07-12 15:05:50 +0800

    分享页修改

--- resources/views/qf/share.blade.php
+++ resources/views/qf/share.blade.php
@@ -8,12 +8,27 @@
 	</head>
 	<body>
 		<div class="share">
-			<img src="{{ asset('qifu/share2/img/bg.png') }}" alt="" class="bg" />
-			<img src="{{ asset('qifu/share2/img/logo.png') }}" alt="" class="logo" />
-			<img src="{{ asset('qifu/share2/img/buyBtn.png') }}" class="buyBtn"/>
-			<div class="popup">
-				<img src="{{ asset('qifu/share2/img/popup.png') }}" alt="" class="popupbg" />
-				<img src="{{ asset('qifu/share2/img/cancel.png') }}" alt="" class="cancel" />
+			<img src="{{asset('qifu/share2/img/bg.png)}}" alt="" class="bg" />
+			<div class="logo1 hidden">
+				<img src="{{asset('qifu/share2/img/logo.png)}}" alt="" class="logo" />
+				<img src="{{asset('qifu/share2/img/logo2.png)}}" alt="" class="qLogo" />
+				<img src="{{asset('qifu/share2/img/line.png)}}" alt="" class="line" />
+			</div>
+			<div class="logo2 ">
+				<img src="{{asset('qifu/share2/img/logo2.png)}}"/>
+			</div>
+			<div class="footer hidden">
+				<img src="{{asset('qifu/share2/img/text.png)}}" class="textPic"/>
+				<img src="{{asset('qifu/share2/img/buyBtn.png)}}" class="buyBtn"/>
+				
+			</div>
+			<div class="footer2 ">
+				<img class="textPic2" src="{{asset('qifu/share2/img/text.png)}}"/>
+			</div>
+
+			<div class="popup hidden">
+				<img src="{{asset('qifu/share2/img/popup.png)}}" alt="" class="popupbg" />
+				<img src="{{asset('qifu/share2/img/cancel.png)}}" alt="" class="cancel" />
 			</div>
 		</div>
 		
@@ -23,5 +38,6 @@
 		$('.popup .cancel').click(function(){
 			$('.popup').hide();
 		})
+		{{ asset('qifu/share2/img/bg.png') }}
 	</script>
 </html>
