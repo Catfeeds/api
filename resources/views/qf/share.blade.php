010aa044ade7b2c226da8acc3c85bd8c04adbf96
zhaoyang <602155776@qq.com>
2017-07-14 10:12:00 +0800

    启赋 share

--- resources/views/qf/share.blade.php
+++ resources/views/qf/share.blade.php
@@ -9,7 +9,7 @@
 <body>
 <div class="share">
     <img src="{{asset('qifu/share2/img/bg.png')}}" alt="" class="bg"/>
-    @if(isset($logo))
+    @if(!is_null($logo))
         <div class="logo1 ">
             <img src="{!! $logo !!}" alt="" class="logo"/>
             <img src="{{asset('qifu/share2/img/logo2.png') }}" alt="" class="qLogo"/>
@@ -20,7 +20,7 @@
             <img src="{{asset('qifu/share2/img/logo2.png')}}"/>
         </div>
     @endif
-    @if(isset($logo))
+    @if(!is_null($logo))
         <div class="footer ">
             <img src="{{asset('qifu/share2/img/text.png')}}" class="textPic"/>
             <a href="{!! $shop_url !!}">
