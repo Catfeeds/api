c5acddbb56e508d137cc08ff17fd1fee46693db1
zhougogo <1290612478@qq.com>
2017-07-15 09:55:16 +0800

    修改手写板签到

--- public/qifu/encouragement/js/index.js
+++ public/qifu/encouragement/js/index.js
@@ -1,13 +1,17 @@
 $(function(){
-    $('.btn').click(function(){
-        if($('.ipt').val() != ''){
-            $('.page1').fadeOut();
-            $('.page2').fadeIn()
-        }else{
-            $('.mask').fadeIn();
-        }
+//  $('.btn').click(function(){
+//      if($('.ipt').val() != ''){
+//          $('.page1').fadeOut();
+//          $('.page2').fadeIn()
+//      }else{
+//          $('.mask').fadeIn();
+//      }
+//  });
+    $('.ipt').click(function(){
+        $('#myCanvas').show().animate({'top':'646px'},1000);
     });
     $('.close').click(function(){
         $('.mask').fadeOut();
+        $('#myCanvas').show();
     });
 });
