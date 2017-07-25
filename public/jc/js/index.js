14cca87eac4d69a24df9fe41562f982c75e6857e
zhougogo <1290612478@qq.com>
2017-07-25 13:45:42 +0800

    add jc

--- public/jc/js/index.js
+++ public/jc/js/index.js
@@ -4,29 +4,29 @@ $(function(){
     // 点击关注我们按钮
 
     $('.content1 .careBtn').click(function(){
-        $('.content1').fadeOut(1000);
-        $('.content2').fadeIn(1000);
+        $('.content1').fadeOut(800);
+        $('.content2').fadeIn(800);
     })
 
     //点击content2的我知道了按钮
 
     $('.content2 .kownBtn').click(function(){
-        $('.content2').fadeOut(1000);
-        $('.content1').fadeIn(1000);
+        $('.content2').fadeOut(800);
+        $('.content1').fadeIn(800);
     })
 
     //点击分享你的gif
 
     $('.content1 .shareBtn').click(function(){
-        $('.content1').fadeOut(1000);
-        $('.content3').fadeIn(1000);
+        $('.content1').fadeOut(800);
+        $('.content3').fadeIn(800);
     })
 
     //点击content3的我知道了按钮
 
     $('.content3 .kownBtn').click(function(){
-        $('.content3').fadeOut(1000);
-        $('.content1').fadeIn(1000);
+        $('.content3').fadeOut(800);
+        $('.content1').fadeIn(800);
     })
 })
 
--- resources/views/jc/index.blade.php
+++ resources/views/jc/index.blade.php
@@ -4,40 +4,57 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=640,target-densitydpi=device-dpi,user-scalable=no">
-		<link rel="stylesheet" href="{{ asset('jc/css/index.css') }}" />
-		<link rel="stylesheet" type="text/css" href="{{ asset('jc/css/zy.media.min.css') }}"/>
+		<link rel="stylesheet" href="{{asset('jc/css/index.css')}}" />
+		<link rel="stylesheet" type="text/css" href="{{asset('jc/css/video-js.css')}}"/>
 		<title></title>
 	</head>
 
 	<body>
 		<div class="page">
 			<div class="content content1">
-					<div class="zy_media">
-						<video x-webkit-airplay="true" playsinline webkit-playsinline="true" autoplay="autoplay">
-							<source src="{!! $path !!}" type="video/mp4"> 您的浏览器不支持HTML5视频
-						</video>
-					</div>
+				<video webkit-playsinline playsinline  preload='true' loop autoplay   controls id="my-video" class="video-js" width="640" height="505" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+					<source src="{!! $path !!}"> 您的浏览器不支持HTML5视频
+				</video>
 
-				<img src="{{ asset('jc/img/shareBtn.png') }}" alt="" class="shareBtn hidden">
-				<img src="{{ asset('jc/img/careBtn.png') }}" alt="" class="careBtn hidden">
+				<img src="{{asset('jc/img/shareBtn.png')}}" alt="" class="shareBtn hidden">
+				<img src="{{asset('jc/img/careBtn.png')}}" alt="" class="careBtn hidden">
 			</div>
 			<div class="content hidden content2">
-				<img src="{{ asset('jc/img/code.png') }}" alt="" class="code">
-				<img src="{{ asset('jc/img/knowBtn.png') }}" alt="" class="kownBtn">
+				<img src="{{asset('jc/img/code.png')}}" alt="" class="code">
+				<img src="{{asset('jc/img/knowBtn.png')}}" alt="" class="kownBtn">
 			</div>
 			<div class="content hidden content3">
-				<img src="{{ asset('jc/img/shareText.png') }}" alt="" class="shareText">
-				<img src="{{ asset('jc/img/plane.png') }}" alt="" class="plane">
-				<img src="{{ asset('jc/img/knowBtn.png') }}" alt="" class="kownBtn">
+				<img src="{{asset('jc/img/shareText.png')}}" alt="" class="shareText">
+				<img src="{{asset('jc/img/plane.png')}}" alt="" class="plane">
+				<img src="{{asset('jc/img/knowBtn.png')}}" alt="" class="kownBtn">
 			</div>
 		</div>
 
 	</body>
-	<script src="{{ asset('jc/js/jquery.min.js') }}"></script>
-	<script src="{{ asset('jc/js/zy.media.min.js') }}" type="text/javascript" charset="utf-8"></script>
-	<script src="{{ asset('jc/js/index.js') }}"></script>
+	<script src="{{asset('jc/js/jquery.min.js')}}"></script>
+	<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+	<script src="{{asset('jc/js/video.min.js')}}" type="text/javascript" charset="utf-8"></script>
+	<script src="{{asset('jc/js/index.js')}}"></script>
 	<script type="text/javascript">
-		zymedia('video', { autoplay: true });
+			var myPlayer = videojs('my-video');
+				videojs("my-video").ready(function() {
+					var myPlayer = this;
+					myPlayer.play();
+					
+					var video = $('video');
+	
+
+			
+					video[0].play(); 
+					document.addEventListener("WeixinJSBridgeReady", function () { 
+						video[0].play();
+ 
+				    }, false);
+				});
+				
+				
+
 	</script>
 
 </html>
+
