a60c3738580a4ba021dafeeb4b5a8f6611a5f7d0
zhaoyang <602155776@qq.com>
2017-07-25 18:25:22 +0800

    视频上传接口

--- resources/views/jc/index.blade.php
+++ resources/views/jc/index.blade.php
@@ -49,21 +49,20 @@
 <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 
 <script type="text/javascript">
-    alert("aa{{ $_SERVER['REQUEST_URI'] }}");
     wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), true) ?>);
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
     wx.ready(function () {
         // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
         wx.onMenuShareTimeline({
             title: '查看我的#Own Your Juicy#专属视频', // 分享标题
-            link: "{{ $_SERVER['REQUEST_URI'] }}",
+            link: "https://api.shanghaichujie.com{{ $_SERVER['REQUEST_URI'] }}",
             imgUrl: "{{asset('jc/img/jc_share.png')}}" // 分享图标
         });
         // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
         wx.onMenuShareAppMessage({
             title: '有人说了一些不是谁都懂的话,来看看你懂吗?', // 分享标题
             desc: "邀你玩转Juicy时尚派对", // 分享描述
-            link: "{{ $_SERVER['REQUEST_URI'] }}",
+            link: "https://api.shanghaichujie.com{{ $_SERVER['REQUEST_URI'] }}",
             imgUrl: "{{asset('jc/img/jc_share.png')}}", // 分享图标
             type: 'link' // 分享类型,music、video或link，不填默认为link
         });
