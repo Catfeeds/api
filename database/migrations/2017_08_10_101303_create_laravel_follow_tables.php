fce8e1886d168896426cde01e281943ec230f12c
zhaoyang <602155776@qq.com>
2017-08-31 18:01:06 +0800

    关注表索引过大

--- database/migrations/2017_08_10_101303_create_laravel_follow_tables.php
+++ database/migrations/2017_08_10_101303_create_laravel_follow_tables.php
@@ -15,7 +15,7 @@ class CreateLaravelFollowTables extends Migration
         Schema::create(config('follow.followable_table', 'followables'), function (Blueprint $table) {
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('followable_id');
-            $table->string('followable_type')->index();
+            $table->string('followable_type', '150')->index();
             $table->string('relation')->default('follow')->comment('folllow/like/subscribe/favorite/');
             $table->timestamp('created_at');
 
