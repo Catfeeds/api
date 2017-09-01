0a9e9a0a398995f53783a8fe60dbd20105d454cb
zhaoyang <602155776@qq.com>
2017-09-01 15:48:37 +0800

    阿里接口

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -25,7 +25,7 @@ class ImageController extends Controller
         //拍照上传10张照片，保存到标识文件夹
         for ($i = 0; $i < 2; $i++) {
             Storage::disk('public_path')
-                ->putFileAs('ali/' . $request->id . '/p' . $i . '.png', $request->file('p' . $i), 'p'.$i.'.png');
+                ->putFileAs('ali/' . $request->id , $request->file('p' . $i), 'p'.$i.'.png');
         }
         //保存信息到数据库
         $ali = new Ali;
