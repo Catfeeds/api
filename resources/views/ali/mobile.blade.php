39a6d280955cade2c3c3bc85225f64abea80514b
zhaoyang <602155776@qq.com>
2017-09-06 22:27:10 +0800

    ali项目

--- resources/views/ali/mobile.blade.php
+++ resources/views/ali/mobile.blade.php
@@ -8,21 +8,30 @@
 </head>
 <body>
 <div class="all">
-    <!--轮播图-->
-    <div id="temp3">
-        <ul class="JQ-slide-content">
-            @for( $i=0;$i<10;$i++)
-                <li>
-                    <img src="{{ asset('upload/ali') .'/'. $ali->uid  . '/p' . $i . '.png' }}"/>
-                </li>
-            @endfor
-        </ul>
-    </div>
+    <!--序列帧-->
+
+    <canvas id='canvas'></canvas>
     <div class="text">
         <p>姓名:<span>{{ $ali->name }}</span></p>
         <p>2018财年累计申报<span>{{ $ali->hours }}</span>公益时</p>
     </div>
 </div>
+{{--<div class="all">--}}
+    {{--<!--轮播图-->--}}
+    {{--<div id="temp3">--}}
+        {{--<ul class="JQ-slide-content">--}}
+            {{--@for( $i=0;$i<10;$i++)--}}
+                {{--<li>--}}
+                    {{--<img src="{{ asset('upload/ali') .'/'. $ali->uid  . '/p' . $i . '.png' }}"/>--}}
+                {{--</li>--}}
+            {{--@endfor--}}
+        {{--</ul>--}}
+    {{--</div>--}}
+    {{--<div class="text">--}}
+        {{--<p>姓名:<span>{{ $ali->name }}</span></p>--}}
+        {{--<p>2018财年累计申报<span>{{ $ali->hours }}</span>公益时</p>--}}
+    {{--</div>--}}
+{{--</div>--}}
 
 </body>
 <script src="{{ asset('alibaba/js/jquery.1.4.2-min.js') }}"></script>
