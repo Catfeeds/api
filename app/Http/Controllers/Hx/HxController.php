4d84e96de5e0ac57e34bf7ea5ea25b86342d0fe9
zhaoyang <602155776@qq.com>
2017-10-25 16:43:46 +0800

    和讯接口

--- app/Http/Controllers/Hx/HxController.php
+++ app/Http/Controllers/Hx/HxController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Hx;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Overtrue\EasySms;
 
 class HxController extends Controller
 {
@@ -11,8 +12,46 @@ class HxController extends Controller
     {
         $id= $request->id;
         return response()->json([
+            'status' => 0,
             'name' => '张国荣',
             'company' => '上海触界数码科技'.$id
         ]);
+    }
+
+    public function sms()
+    {
+        $config = [
+            // HTTP 请求的超时时间（秒）
+            'timeout' => 5.0,
+
+            // 默认发送配置
+            'default' => [
+                // 网关调用策略，默认：顺序调用
+//                'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+                // 默认可用的发送网关
+                'gateways' => [
+                    'aliyun',
+                ],
+            ],
+            // 可用的网关配置
+            'gateways' => [
+                'aliyun' => [
+                    'access_key_id' => 'LTAIBA9gKn4R2Zap',
+                    'access_key_secret' => 'JGrYLZeYL4vQYIc0bKCAzCbuR5zEbt',
+                    'sign_name' => '阿里云短信测试专用',
+                ],
+            ],
+        ];
+        $easySms = new EasySms\EasySms($config);
+
+        $easySms->send(13331936826, [
+            'content'  => '您的验证码为: 6379',
+            'template' => 'SMS_82255160',
+            'data' => [
+                'customer' => '666'
+            ],
+        ]);
+        dd($easySms);
     }
 }
