043059705824205d311b648d1f39c6224333b1b5
zhaoyang <602155776@qq.com>
2017-10-26 16:29:45 +0800

    赛诺菲星球-深圳

--- public/Planet/s_earth/js/main.js
+++ public/Planet/s_earth/js/main.js
@@ -487,7 +487,7 @@
 
 
         // 图片路径前缀
-        Config.imgPath = 'http://api.touchworld-sh.com/Plant/s_earth/img/';
+        Config.imgPath = 'http://api.touchworld-sh.com/Planet/s_earth/img/';
 
         Config.meteorImg = Config.imgPath + 'bg_meteor.png';
 
