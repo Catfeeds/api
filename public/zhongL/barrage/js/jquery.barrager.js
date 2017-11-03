f1fbc0eec15423302e7da106913afc6b676146bc
LOLO <279144728@qq.com>
2017-11-03 15:22:24 +0800

    speed

--- public/zhongL/barrage/js/jquery.barrager.js
+++ public/zhongL/barrage/js/jquery.barrager.js
@@ -44,7 +44,7 @@
 		function barrager() {
 			var window_width = $('body').width() + 500;
 			if (i < window_width) {
-				i += 1;
+				i += 5;
 				$(id).css('margin-right', i);
 			} else {
 				clearInterval(looper);
