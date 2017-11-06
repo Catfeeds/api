b934f9c44f826ac61e646b3938d1f571b4d17930
zhaoyang <602155776@qq.com>
2017-11-06 17:04:23 +0800

    MyLike 180度拍照

--- resources/views/myLike/index2.blade.php
+++ resources/views/myLike/index2.blade.php
@@ -114,7 +114,7 @@ $(function () {
             id: $('#canvas1')[0],
             width: 480,
             height: 270,
-            speed: 100,
+            speed: 200,
             loop: true,
             autoplay: true,
             imgArr: imgarr
