c6ddaf0be718abe0a7290cfc9c8b7bb1b8a50e60
zhaoyang <602155776@qq.com>
2017-11-06 09:29:47 +0800

    MyLike 180度拍照

--- app/Http/Controllers/MyLike/ApiController.php
+++ app/Http/Controllers/MyLike/ApiController.php
@@ -11,6 +11,12 @@ class ApiController extends Controller
     public function upload(Request $request)
     {
         $uid = $request->uid;
+        if (is_null($uid)){
+            return 'false';
+        }
+        if(is_null($request->file('photo'))){
+            return 'false';
+        }
         Storage::disk('public_path')->putFileAS('myLike', $request->file('photo'), $uid.'.gif');
         return env('APP_URL') . '/myLike/?uid=' . $uid;
     }
