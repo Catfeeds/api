2c069d20aa15fe96a399b7f427933b133f045d0d
zhaoyang <602155776@qq.com>
2017-11-06 10:43:47 +0800

    MyLike 180度拍照

--- app/Http/Controllers/MyLike/IndexController.php
+++ app/Http/Controllers/MyLike/IndexController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers\MyLike;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use EasyWeChat\Foundation\Application;
 
 class IndexController extends Controller
 {
+    public $js;
+
+    /**
+     * AliController constructor.
+     * @param Application $app
+     */
+
+    public function __construct(Application $app)
+    {
+        $this->js = $app->js;
+    }
+
     public function index(Request $request)
     {
+        $js = $this->js;
         $uid = $request->uid;
-        return view('myLike.index',compact('uid'));
+        return view('myLike.index', compact('uid', 'js'));
     }
 }
