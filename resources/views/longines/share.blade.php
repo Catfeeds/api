5e963156d96fa169fbc83d04b37a5c6ea872ea68
zhaoyang <602155776@qq.com>
2017-12-04 10:31:21 +0800

    浪琴

--- resources/views/longines/share.blade.php
+++ resources/views/longines/share.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=640, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="x5-fullscreen" content="true">
     <meta name="full-screen" content="yes">
     <title>LONGINES</title>
