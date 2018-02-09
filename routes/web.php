5d99532f97a55ec84ea1a569711be91be1439484
zhaoyang <602155776@qq.com>
2018-02-09 11:07:12 +0800

    凯翼汽车h5

--- routes/web.php
+++ routes/web.php
@@ -204,3 +204,6 @@ Route::group(['middleware' => ['web', 'wechat.oauth:snsapi_userinfo']], function
     Route::post('cowin/greeting', 'Cowin\IndexController@greeting');
 });
 Route::get('cowin/share/{id}', 'Cowin\IndexController@share');
+Route::get('ky/index', function (){
+    return redirect('http://cs.touchworld-sh.com/lolo/cowin_auto_guide/');
+});
