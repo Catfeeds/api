10a388b1115315efaf989b3efcb12ef12d3f639d
zhaoyang <602155776@qq.com>
2018-02-27 08:22:48 +0800

    cowin h5

--- resources/views/cowin/guide.blade.php
+++ resources/views/cowin/guide.blade.php
@@ -174,7 +174,7 @@
 
     //进入年会
     $('.welcome button').on('touchstart', function () {
-        @if(is_null($user))
+        @if($user->confirm)
         $('.sign').fadeIn().siblings().hide();
         @else
         $('.select').fadeIn().siblings().hide();
