a9f901c02bbdab0f00fc6131d12baa85384cb3ea
zhaoyang <602155776@qq.com>
2018-03-26 15:33:08 +0800

    friso 项目

--- resources/views/friso/pyj.blade.php
+++ resources/views/friso/pyj.blade.php
@@ -12,7 +12,7 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide page1">
                 <img class="bg" src="{!! env('upload_url') . '/' . $path !!}" alt="">
-                <a class="btn" href="http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward">
+                <a class="btn" href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd32fafd4a4fd991a&redirect_uri=http%3a%2f%2ffrisowechat.rfc-china.com%2fWebService%2fVendorAuthCB.aspx%3fVendorId%3d1073&response_type=code&scope=snsapi_base&state=0#wechat_redirect">
                     <img  src="{{ asset('res/friso/pyj/img/btn.png') }}" alt="">
                 </a>
                 <p>长按保存图片</p>
