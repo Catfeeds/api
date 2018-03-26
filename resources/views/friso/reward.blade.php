4c5f65dd78623407b1ba0ce62e7d456ca619b3ea
zhougogo <1290612478@qq.com>
2018-03-26 15:47:39 +0800

    上传美素

--- resources/views/friso/reward.blade.php
+++ resources/views/friso/reward.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=640, initial-scale=1.0">
+    <meta name="viewport" content="width=640, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>美素佳儿</title>
     <link rel="stylesheet" href="{{ asset('res/friso/reward/css/index.css') }}">
