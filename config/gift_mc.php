7385de29abb66e907c653baa03d2a494220b078a
zhaoyang <602155776@qq.com>
2018-04-13 10:35:14 +0800

    名爵车展api接口

--- config/gift_mc.php
+++ config/gift_mc.php
@@ -1,21 +1,8 @@
 <?php
 
 return [
-    'sign' => 10,
-    'discover' => 10,
-    'ar' => 10,
-    'car' => 10,
-    'show' => 10,
-    'gift1' => 10,
-    'gift2' => 10,
-    'gift3' => 10,
-    'gift4' => 10,
-    'gift5' => 10,
-    'gift6' => 10,
-    'gift7' => 10,
-    'gift8' => 10,
-    'gift9' => 10,
-    'gift10' => 10,
-    'gift11' => 10,
-    'gift12' => 10,
+    'sign' => 20,
+    'discover' => 15,
+    'ar' => 30,
+    'show' => 50,
 ];
