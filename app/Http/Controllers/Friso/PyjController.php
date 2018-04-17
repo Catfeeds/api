8ba66930fc1c2634df5d75aa9424a9ab36839e90
zhaoyang <602155776@qq.com>
2018-04-17 13:32:36 +0800

    美素佳儿接口调整

--- app/Http/Controllers/Friso/PyjController.php
+++ app/Http/Controllers/Friso/PyjController.php
@@ -22,21 +22,65 @@ class PyjController extends Controller
             'base_uri' => 'https://gw.rfc-china.com/',
             'timeout' => 5.0,
         ]);
+
+        //获取access—token
         $res = $client->request('GET', 'api/sso/access_token?appid=demo.TangJi&appsecret=demo.TangJi');
         $body = $res->getBody();
         $code = json_decode((string)$body)->data;
-        $res = $client->request('GET', 'api/customer/customer/getcustomer?openid=' . $openid, [
+
+        //查询会员接口
+//        $res = $client->request('GET', 'api/customer/customer/getcustomer?openid=' . $openid, [
+//            'headers' => [
+//                'authorization' => 'bearer ' . $code,
+//            ]
+//        ]);
+//        $body = $res->getBody();
+//        $data = json_decode((string)$body)->data;
+//        if (is_null($data)) {
+//            return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs
+//');
+//        }
+
+        $res = $client->request('POST', 'api/common/GiftApply/VerifyGiftApplication', [
             'headers' => [
                 'authorization' => 'bearer ' . $code,
-            ]
+            ],
+            'json' => [
+                'Campaigncode' => 'LY_Trial',
+                'CampaignBrand' => 'CampaignBrand',
+                'Giftcode' => 'LY-trialF3',
+                'GiftQty' => 1,
+                'OpenId' => $openid
+            ],
         ]);
-        $body = $res->getBody();
-        $data = json_decode((string)$body)->data;
-
-        if (is_null($data)) {
-            return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs
-');
+        switch (json_decode((string)$res->getBody())->data) {
+            case 0:
+                //可以申领
+                return view('friso.reward', compact('openid'));
+                break;
+            case 1:
+                //已经申领
+                $warning = '您已经领取过该礼品';
+                return view('friso.warning', compact('warning'));
+                break;
+            case 2:
+                //库存不足
+                $warning = '礼品库存不足，请联系工作人员';
+                return view('friso.warning', compact('warning'));
+                break;
+            case 3:
+                //不符合条件
+                $warning = '请到现场工作人员处领取';
+                return view('friso.warning', compact('warning'));
+                break;
+            case 5:
+                //非会员
+                $warning = '请先注册会员';
+                return view('friso.warning', compact('warning'));
+                break;
+            default:
+                $warning = '出现错误，请联系工作人员解决';
+                return view('friso.warning', compact('warning'));
         }
-        return view('friso.reward', compact('openid'));
     }
 }
---
+++ resources/views/friso/warning.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=640,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>美素佳儿</title>
+    <link rel="stylesheet" href="{{ asset('res/friso/warning/css/index.css') }}">
+</head>
+<body>
+    <div class="all">
+        <section>
+            <p>{{ $warning }}</p>
+        </section>
+    </div>
+</body>
+</html>
