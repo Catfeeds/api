56e37348d4c98e1f8aa0fd451f9ffaba9848abde
zhaoyang <602155776@qq.com>
2018-04-23 15:38:30 +0800

    名爵车展

--- app/Http/Controllers/Friso/PyjController.php
+++ app/Http/Controllers/Friso/PyjController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Friso;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
@@ -29,59 +30,71 @@ class PyjController extends Controller
         $code = json_decode((string)$body)->data;
 
         //查询会员接口
-//        $res = $client->request('GET', 'api/customer/customer/getcustomer?openid=' . $openid, [
-//            'headers' => [
-//                'authorization' => 'bearer ' . $code,
-//            ]
-//        ]);
-//        $body = $res->getBody();
-//        $data = json_decode((string)$body)->data;
-//        if (is_null($data)) {
-//            return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs
-//');
-//        }
-
-        $res = $client->request('POST', 'api/common/GiftApply/VerifyGiftApplication', [
+        $res = $client->request('GET', 'api/customer/customer/getcustomer?openid=' . $openid, [
             'headers' => [
                 'authorization' => 'bearer ' . $code,
-            ],
-            'json' => [
-                'Campaigncode' => 'LY_Trial',
-                'CampaignBrand' => 'CampaignBrand',
-                'Giftcode' => 'LY-trialF3',
-                'GiftQty' => 1,
-                'OpenId' => $openid
-            ],
+            ]
         ]);
-        switch (json_decode((string)$res->getBody())->data) {
-            case 0:
-                //可以申领
-                return view('friso.reward', compact('openid'));
-                break;
-            case 1:
-                //已经申领
-                $warning = '您已经领取过该礼品';
-                return view('friso.warning', compact('warning'));
-                break;
-            case 2:
-                //库存不足
-                $warning = '礼品库存不足，请联系工作人员';
-                return view('friso.warning', compact('warning'));
-                break;
-            case 3:
-                //不符合条件
-                $warning = '请到现场工作人员处领取';
-                return view('friso.warning', compact('warning'));
-                break;
-            case 5:
-                //非会员
-                $warning = '请先注册会员';
-                return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs');
-                //return view('friso.warning', compact('warning'));
-                break;
-            default:
-                $warning = '出现错误，请联系工作人员解决';
-                return view('friso.warning', compact('warning'));
+        $body = $res->getBody();
+        $data = json_decode((string)$body)->data;
+        if (is_null($data)) {
+            return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs
+');
         }
+        $baby = $data->CustomerBaby;
+        $birthday = new Carbon($baby[0]->BabyBirthday);
+
+        $now = Carbon::now();
+        $d1 = $now->copy()->subDay(1095);
+        $d2 = $now->copy()->subDay(365);
+        if ($birthday->between($d1, $d2)) {
+            return view('friso.reward', compact('openid'));
+        }
+        $warning = '请到现场工作人员处领取';
+        return view('friso.warning', compact('warning'));
+
+
+//        $res = $client->request('POST', 'api/common/GiftApply/VerifyGiftApplication', [
+//            'headers' => [
+//                'authorization' => 'bearer ' . $code,
+//            ],
+//            'json' => [
+//                'Campaigncode' => 'LY_Trial',
+//                'CampaignBrand' => 'CampaignBrand',
+//                'Giftcode' => 'LY-trialF3',
+//                'GiftQty' => 1,
+//                'OpenId' => $openid
+//            ],
+//        ]);
+//        switch (json_decode((string)$res->getBody())->data) {
+//            case 0:
+//                //可以申领
+//                return view('friso.reward', compact('openid'));
+//                break;
+//            case 1:
+//                //已经申领
+//                $warning = '您已经领取过该礼品';
+//                return view('friso.warning', compact('warning'));
+//                break;
+//            case 2:
+//                //库存不足
+//                $warning = '礼品库存不足，请联系工作人员';
+//                return view('friso.warning', compact('warning'));
+//                break;
+//            case 3:
+//                //不符合条件
+//                $warning = '请到现场工作人员处领取';
+//                return view('friso.warning', compact('warning'));
+//                break;
+//            case 5:
+//                //非会员
+//                $warning = '请先注册会员';
+//                return redirect('http://frisowechat.rfc-china.com/frontPage/Reg.aspx?regsourceid=180&retUrl=https%3a%2f%2fapi.shanghaichujie.com%2ffriso%2freward%3Fopenid=oSG6Njto383u5YW9KCm0DyT0uCzs');
+//                //return view('friso.warning', compact('warning'));
+//                break;
+//            default:
+//                $warning = '出现错误，请联系工作人员解决';
+//                return view('friso.warning', compact('warning'));
+//        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 */
 
 Route::get('test', function () {
-//    return phpinfo();
+
 });
 
 /*
