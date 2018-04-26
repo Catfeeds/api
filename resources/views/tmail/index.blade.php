7bfc84bfd850797f25525dce87a0fb49a2d8f232
zhaoyang <602155776@qq.com>
2018-04-26 11:08:58 +0800

    tmail

--- resources/views/tmail/index.blade.php
+++ resources/views/tmail/index.blade.php
@@ -119,7 +119,7 @@
 </section>
 
 <!-- 全局通用 -->
-<p class="footer hide">活动最终解释权归主办方所有</p>
+<p class="footer hide" >活动最终解释权归主办方所有</p>
 <img class="luck_btn hide" src="../../res/tmail/images/luck_btn.png">
 
 <!-- 积分 -->
@@ -197,7 +197,30 @@
 </body>
 <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="application/javascript">
-
+    wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
+    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
+    wx.ready(function () {
+        // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
+        wx.onMenuShareTimeline({
+            title: '奇屋妙想', // 分享标题
+            link: "{{ url('tmail/index') }}",
+            imgUrl: "{{ url('res/tmail/share.jpg') }}", // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            }
+        });
+        // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
+        wx.onMenuShareAppMessage({
+            title: '奇屋妙想', // 分享标题
+            desc: "大牌们的N次方", // 分享描述
+            link: "{{ url('tmail/index') }}",
+            imgUrl: "{{ url('res/tmail/share.jpg') }}", // 分享图标
+            type: 'link', // 分享类型,music、video或link，不填默认为link
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            }
+        });
+    });
     function statistic(type) {
         //统计进入人数
         $.get('{{ url('api/tmail/statistic') }}?type=' + type);
