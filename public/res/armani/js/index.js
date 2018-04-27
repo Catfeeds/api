f60b93105546e6c7d6256a06d182cb66c442897c
zhougogo <1290612478@qq.com>
2018-04-27 17:16:58 +0800

    阿玛尼

--- public/res/armani/js/index.js
+++ public/res/armani/js/index.js
@@ -13,7 +13,7 @@ window.onload = function(){
         if (isWeixin){
             if (/iphone|ipad|ipod/.test(ua)) {
                 // alert("iphone");
-                $(this).css('href','');
+                $(this).css('href','#');
                 $('.ipopup').show();
                 $('.apopup').hide();
             } else if (/android/.test(ua)) {
