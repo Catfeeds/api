2cff2f5536099244d194152f2a8a441d4761ca7e
zhaoyang <602155776@qq.com>
2018-08-15 18:20:08 +0800

    absolut 合成图片

--- app/Http/Controllers/Absolut/ApiController.php
+++ app/Http/Controllers/Absolut/ApiController.php
@@ -26,32 +26,31 @@ class ApiController extends Controller
 
         if ($platform == 'pad') {
             //上传源文件
-            Image::make($img)->save(public_path('upload/absolut/'.$uniqid.'.png'));
-            $path = Storage::disk('oss')->putFileAs('absolut', new File(public_path('upload/absolut/'.$uniqid.'.png')), uniqid().'.png');
+            Image::make($img)->save(public_path('upload/absolut/' . $uniqid . '.png'));
+            $path = Storage::disk('oss')->putFileAs('absolut', new File(public_path('upload/absolut/' . $uniqid . '.png')), uniqid() . '.png');
             //保存pad端源文件用于打印
             $item = new Absolut();
             $item->locationId = $lid;
-            $item->imgUrl = config('filesystems.disks.oss.oss_url').$path;
+            $item->imgUrl = config('filesystems.disks.oss.oss_url') . $path;
             $item->save();
 
         }
         $base = Image::make(public_path('res/absolut/base_bg.png'));
-        $img =Image::make($img)->resize(1767,2473);
+        $img = Image::make($img)->resize(1767, 2473);
 
-        $base->insert($img, 'top-left', 154, 114)->save(public_path('upload/absolut/'.$uniqid.'.png'));
+        $base->insert($img, 'top-left', 154, 114)->save(public_path('upload/absolut/' . $uniqid . '.png'));
 
-        $path = Storage::disk('oss')->putFileAs('absolut', new File(public_path('upload/absolut/'.$uniqid.'.png')), uniqid().'.png');
+        $path = Storage::disk('oss')->putFileAs('absolut', new File(public_path('upload/absolut/' . $uniqid . '.png')), uniqid() . '.png');
 
 
-
-        return config('filesystems.disks.oss.oss_url').$path;
+        return config('filesystems.disks.oss.oss_url') . $path;
     }
 
     public function printImg(Request $request)
     {
         $absout = Absolut::where('locationId', $request->location_id)
             ->where('status', 0)
-            ->where('created_at', '>',  Carbon::today())
+            ->where('created_at', '>', Carbon::today())
             ->first();
         if ($absout) {
             $absout->status = 1;
