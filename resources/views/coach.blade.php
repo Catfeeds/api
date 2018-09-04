a31f7b8947adf78700a4e3b4dcfca9c6bfa2e9f4
chenzhaoyang <602155776@qq.com>
2018-09-04 11:33:16 +0800

    coach 提取照片

--- resources/views/coach.blade.php
+++ resources/views/coach.blade.php
@@ -13,56 +13,60 @@
     <script src="//cdn.jsdelivr.net/npm/v-charts/lib/index.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/iview/3.0.1/styles/iview.css" rel="stylesheet">
     <link rel="stylesheet" href="//res.wx.qq.com/open/libs/weui/1.1.3/weui.min.css"/>
+    <style>
+        body {
+            width: 640px;
+            height: 1038px;
+            background: url("https://unitytouch.oss-cn-shanghai.aliyuncs.com/Fred%2Fcoach%2F0.jpg") no-repeat;
+            background-size: cover;
+            /*background-color: #C49487;*/
+        }
+        .photo-input {
+            position: absolute;
+            width: 320px;
+            left:0;
+            right: 0;
+            margin:auto;
+            top:430px;
+            text-align: center;
+        }
+        .verify {
+            margin-bottom: 80px;
+            width: 320px;
+            border: none;
+            height: 80px;
+            line-height: 80px;
+            border: 4px solid #C49487;
+            font-size: 35px;
+            color: #102c4f;
+            box-sizing: border-box;
+            background-color: #D3D3D3;
+        }
+    </style>
 </head>
 <body>
-
 <div id="app">
-    <img width="640px" v-bind:src="url"
-         alt="">
+    <div class="photo-input" id="photo-input">
+        <input type="text" class="verify" required id="photo" placeholder="请输入照片提取码">
+        <img src="https://unitytouch.oss-cn-shanghai.aliyuncs.com/Fred%2Fcoach%2Fsubmit.png" alt="" onclick="f()">
+    </div>
+    <div>
+        <img id="img" width="640px" src="" alt="">
+    </div>
 </div>
 </body>
 <script type="application/javascript">
+    function f() {
 
+        let photo = document.getElementById("photo").value;
+        if (photo) {
+            document.querySelector("body").style.background= 'url("") no-repeat';
+            document.getElementById('img').src = 'https://unitytouch.oss-cn-shanghai.aliyuncs.com/Fred%2Fcoach%2F'
+                + photo + '.png';
+            document.getElementById("photo-input").style.display = "none";
+        }
 
-    var Main = {
-        data() {
-            return {
-                value: 0,
-                inputVal:''
-            }
-        },
-        computed: {
-            url: function () {
-                return 'https://unitytouch.oss-cn-shanghai.aliyuncs.com/Fred%2Fcoach%2F' + this.value + '.png'
-            }
-        },
-        mounted: function () {
-            this.$Modal.confirm({
-                width: '50px',
-                title: '请输入照片提取码',
-                cancelText: ' ',
-                onOk: () => this.value = this.inputVal,
-
-                render: (h) => {
-                    return h('Input', {
-                        props: {
-                            value: this.inputVal,
-                            autofocus: true,
-
-                            placeholder: '请输入照片提取码....'
-                        },
-                        on: {
-                            input: (val) => {
-                                this.inputVal = val;
-                            }
-                        }
-                    })
-                }
-            })
-        },
     }
-    var Component = Vue.extend(Main)
-    new Component().$mount('#app')
 </script>
 
 </html>
