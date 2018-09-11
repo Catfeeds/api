e51b72fa5902026af27c126c63aff7a8b65eca6b
chenzhaoyang <602155776@qq.com>
2018-09-11 14:34:27 +0800

    欧莱雅h5

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,21 +1,51 @@
 <!DOCTYPE html>
 <html lang="zh_cn">
 <head>
-  <meta charset="UTF-8">
-  <title></title>
+    <meta charset="UTF-8">
+    <title></title>
 
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 
 </body>
 
-<script src="//localhost:6001/socket.io/socket.io.js"></script>
-<script src="{{ asset('js/app.js') }}" type="text/javascript" charset="utf-8"></script>
+
 <script type="application/javascript">
-    Echo.channel('Zl')
-        .listen('ZlSign', (e) => {
-            console.log(e.nickname);
-        });
+    function ksort(obj){
+        let keys = Object.keys(obj).sort()
+            , sortedObj = {};
+
+        for(let i in keys) {
+            sortedObj[keys[i]] = obj[keys[i]];
+        }
+
+        return sortedObj;
+    }
+    let arr = {
+        'app_id': '2108118185',
+        'image' : '',
+        'cosmetic' : 1,
+        'time_stamp' : new Date().getTime(),
+        'nonce_str' : Math.round(Math.random()*100000),
+        'sign' : '',
+    };
+    function getSign(obj, appkey) {
+        obj = ksort(obj);
+
+        let str = '';
+
+        for (let key in obj) {
+            if (obj[key] !== '') {
+                str += key + '=' + encodeURI(obj[key]) +'&'
+            }
+        }
+        str += 'app_key=' + appkey;
+
+        str = md5(str).toUpperCase();
+
+        return str;
+    }
+    arr['sign'] = getSign(arr, 'app_key')
 </script>
 </html>
