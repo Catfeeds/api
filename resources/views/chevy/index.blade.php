892c4ad3fbc86047c0b736a9b48831400959eff4
chenzhaoyang <602155776@qq.com>
2018-09-11 15:53:27 +0800

    雪佛兰

--- resources/views/chevy/index.blade.php
+++ resources/views/chevy/index.blade.php
@@ -55,26 +55,12 @@
 </section>
 <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="application/javascript">
-    wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), true) ?>);
+    wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
     wx.ready(function () {
         // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
-        wx.onMenuShareTimeline({
-            title: '雪佛兰电音节', // 分享标题
-            link: 'https://m.chevrolet.com.cn/act/ultramusic/Auth.aspx?scope=snsapi_userinfo&redirect_uri=https://api.shanghaichujie.com/res/chevy/index',
-            imgUrl: "{{ asset('res/chevy/share.png') }}", // 分享图标
-            success: function () {
-            }
-        });
-        // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
-        wx.onMenuShareAppMessage({
-            title: '雪佛兰电音节', // 分享标题
-            desc: "当狂热电音遇见雪佛兰", // 分享描述
-            link: 'https://m.chevrolet.com.cn/act/ultramusic/Auth.aspx?scope=snsapi_userinfo&redirect_uri=https://api.shanghaichujie.com/res/chevy/index',
-            imgUrl: "{{ asset('res/chevy/share.png') }}", // 分享图标
-            type: 'link', // 分享类型,music、video或link，不填默认为link
-            success: function () {
-            }
+        wx.hideMenuItems({
+            menuList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
         });
     });
     var openid = '{{ $user->openid }}'
