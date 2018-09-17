ebbdfeacd44f1bec69743f11c2c9810d0f526529
chenzhaoyang <602155776@qq.com>
2018-09-17 15:41:47 +0800

    天猫精灵

--- app/Http/Controllers/Tmall/GenieController.php
+++ app/Http/Controllers/Tmall/GenieController.php
@@ -9,11 +9,22 @@ use App\Http\Controllers\Controller;
 
 class GenieController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
+     * 战队登记
+     */
     public function index()
     {
         return view('tmall.sign');
     }
 
+    /**
+     * @param Request $request
+     * @return mixed
+     *
+     * 提交登记信息
+     */
     public function sign(Request $request)
     {
         $phones = $request->input('phones');
@@ -27,35 +38,78 @@ class GenieController extends Controller
         return $genie->id;
     }
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * 返回当前战队信息
+     */
     public function genieTime()
     {
         $genie = TmallGenie::where('end', null)
             ->orderBy('created_at')
             ->first();
-        $now = Carbon::now()->addMinutes($genie ? $genie->punish : 0);
+        if ($genie) {
+            $now = Carbon::now()->addMinutes($genie->punish);
+            $minutes = intval($now->copy()->diffInSeconds($genie->created_at) / 60);
+            $seconds = $now->copy()->diffInSeconds($genie->created_at) % 60;
+            if ($minutes < 10) {
+                $minutes = '0' . $minutes;
+            }
+            if ($seconds < 10) {
+                $seconds = '0' . $seconds;
+            }
+            return response()->json([
+                'code' => 1,
+                'genie' => [
+                    'name' => $genie->name,
+                    'time' => $minutes . ':' . $seconds
+                ],
+            ]);
+        }
         return response()->json([
-            'code' => $genie ? 1 : 0,
-            'genie' => $genie ? [
-                'id' => $genie->id,
-                'name' => $genie->name . $genie->id,
-                'time' => intval($now->copy()->diffInSeconds($genie->created_at) / 60) . ':' . $now->copy()->diffInSeconds($genie->created_at) % 60
-            ] : null,
+            'code' => 0,
+            'genie' => null,
         ]);
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * 点击任务完成
+     */
     public function confirm($id)
     {
         $genie = TmallGenie::find($id);
         $genie->end = Carbon::now();
         $genie->save();
 
-        $now = Carbon::now()->addMinutes($genie ? $genie->punish : 0);
+        if ($genie) {
+            $now = Carbon::now()->addMinutes($genie->punish);
+            $minutes = intval($now->copy()->diffInSeconds($genie->created_at) / 60);
+            $seconds = $now->copy()->diffInSeconds($genie->created_at) % 60;
+            if ($minutes < 10) {
+                $minutes = '0' . $minutes;
+            }
+            if ($seconds < 10) {
+                $seconds = '0' . $seconds;
+            }
+            return response()->json([
+                'code' => 1,
+                'genie' => [
+                    'name' => $genie->name,
+                    'time' => $minutes . ':' . $seconds
+                ],
+            ]);
+        }
         return response()->json([
-            'code' => $genie ? 1 : 0,
-            'genie' => $genie ? [
-                'name' => $genie->name,
-                'time' => intval($now->copy()->diffInSeconds($genie->created_at) / 60) . ':' . $now->copy()->diffInSeconds($genie->created_at) % 60
-            ] : null,
+            'code' => 0,
+            'genie' => null,
         ]);
+    }
+
+    public function control()
+    {
+        return view('tmall.control');
     }
 }
