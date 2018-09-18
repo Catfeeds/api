344c40e8949b491a30cf0511cea412b81a60d707
LOLO <279144728@qq.com>
2018-09-18 19:27:18 +0800

    平安云加速器

--- resources/views/pingAn/zh.blade.php
+++ resources/views/pingAn/zh.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
-  <title>平安云加速器D-day</title>
+  <title>平安云加速器D-Day</title>
   <script src="js/rem.js"></script>
   <link rel="stylesheet" href="css/animate.min.css">
   <link rel="stylesheet" href="css/style5.css">
