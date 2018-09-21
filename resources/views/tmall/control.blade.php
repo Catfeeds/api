6ab7cc4c54c4ec6af81db2730941dd6a72bff0ea
LOLO <279144728@qq.com>
2018-09-21 19:32:57 +0800

    天猫精灵

--- resources/views/tmall/control.blade.php
+++ resources/views/tmall/control.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
     <meta name="msapplication-tap-highlight" content="no"/>
-    <title>战队报名</title>
+    <title>惩罚控制</title>
     <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap/dist/css/bootstrap.min.css"/>
     <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css"/>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
