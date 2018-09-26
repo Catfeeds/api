1f1cf7aa2801854153615f21391cc3082e181aea
zhaoyang017 <602155776@qq.com>
2018-09-26 11:23:33 +0800

    平安

--- app/Http/Controllers/PingAn/IndexController.php
+++ app/Http/Controllers/PingAn/IndexController.php
@@ -15,7 +15,8 @@ class IndexController extends Controller
 
     public function en()
     {
-        $js = \EasyWeChat::js();
-        return view('pingAn.en', compact('js'));
+        return redirect('http://xg.touchworld-sh.com/res/pingAn/en/index');
+//        $js = \EasyWeChat::js();
+//        return view('pingAn.en', compact('js'));
     }
 }
