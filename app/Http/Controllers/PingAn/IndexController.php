29690024c239c0d3b963d9e940bbd9046031473e
zhaoyang017 <602155776@qq.com>
2018-10-15 12:04:55 +0800

    平安 关闭报名

--- app/Http/Controllers/PingAn/IndexController.php
+++ app/Http/Controllers/PingAn/IndexController.php
@@ -10,12 +10,14 @@ class IndexController extends Controller
     public function zh()
     {
         $js = \EasyWeChat::js();
-        return view('pingAn.zh', compact('js'));
+        return '很遗憾！报名已经截止';
+//        return view('pingAn.zh', compact('js'));
     }
 
     public function en()
     {
-        return redirect('http://xg.touchworld-sh.com/res/pingAn/en/index');
+        return '很遗憾！报名已经截止';
+//        return redirect('http://xg.touchworld-sh.com/res/pingAn/en/index');
 //        $js = \EasyWeChat::js();
 //        return view('pingAn.en', compact('js'));
     }
