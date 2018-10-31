2f59ec9f7b96b0136f235067c933588f5451bd4c
LOLO <279144728@qq.com>
2018-10-31 22:29:47 +0800

    雅培

--- public/res/abbott/js/index.js
+++ public/res/abbott/js/index.js
@@ -13,13 +13,25 @@ var realLoadingNum = 0
 var fakeLoadingNum = 0
 var loader = new resLoader({
   resources: [
-    './img/01.jpg',
-    './img/01_text.png',
-    './img/02.jpg',
-    './img/02_text.png',
-    './img/03.jpg',
-    './img/03_text.png',
-    './img/04.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/01_new.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/01_text.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/02_new.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/02_text.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/03_new.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/03_text.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/04_new.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/arrow_bottom.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/logo.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/logo2.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/01_content.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/02_content.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/03_content1.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/03_content2.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/bg.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/loading.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/poster.jpg',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/title.png',
+    'https://h5-touch.oss-cn-shanghai.aliyuncs.com/abbott/video_bg.jpg',
   ],
   onStart: function(total) {
     console.log('加载图片数:' + total);
@@ -58,3 +70,24 @@ var myLoadingInterval = setInterval(function() {
     })
   }
 }, 30)
+
+new Swiper('.swiper-container', {
+  direction: 'vertical', // 垂直切换选项
+  on: {
+    init: function() {
+      swiperAnimateCache(this); //隐藏动画元素 
+      swiperAnimate(this); //初始化完成开始动画
+    },
+    slideChangeTransitionEnd: function() {
+      swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+    }
+  }
+})
+
+var video = document.querySelector('video')
+video.onplay = function () {
+  BGM.pause()
+}
+video.onpause = function () {
+  BGM.load()
+}
