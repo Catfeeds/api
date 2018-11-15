3c46b2fd2dfbe2e9a7a692aca35e60b79ab79961
zhaoyang017 <602155776@qq.com>
2018-11-15 09:36:03 +0800

    上海马拉松

--- resources/views/shangma.blade.php
+++ resources/views/shangma.blade.php
@@ -88,8 +88,33 @@
     <button>下载</button>
   </div>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-  <script>
-    var dom_btn = document.querySelector('button')
+  <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
+  <script type="application/javascript">
+      wx.config(<?php echo $js->jssdk->buildConfig(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
+      // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
+      wx.ready(function () {
+          // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
+          wx.onMenuShareTimeline({
+              title: '上马之嘉', // 分享标题
+              link: window.location.href,
+              imgUrl: "https://h5-touch.oss-cn-shanghai.aliyuncs.com/logo/%E4%B8%8A%E6%B5%B7%E9%A9%AC%E6%8B%89%E6%9D%BE.png", // 分享图标
+              success: function () {
+                  // 用户确认分享后执行的回调函数
+              }
+          });
+          // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
+          wx.onMenuShareAppMessage({
+              title: '上马之嘉', // 分享标题
+              desc: "  ", // 分享描述
+              link: window.location.href,
+              imgUrl: "https://h5-touch.oss-cn-shanghai.aliyuncs.com/logo/%E4%B8%8A%E6%B5%B7%E9%A9%AC%E6%8B%89%E6%9D%BE.png", // 分享图标
+              type: 'link', // 分享类型,music、video或link，不填默认为link
+              success: function () {
+                  // 用户确认分享后执行的回调函数
+              }
+          });
+      });
+      var dom_btn = document.querySelector('button')
     dom_btn.onclick = function() {
     var email = prompt('请输入邮箱');
     var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
