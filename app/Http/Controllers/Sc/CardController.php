e2245e3770a3803832721bcad44b75f4e8b0c7c8
zhaoyang017 <602155776@qq.com>
2018-11-23 15:38:14 +0800

    20181123 渣打银行梦想留言机

--- app/Http/Controllers/Sc/CardController.php
+++ app/Http/Controllers/Sc/CardController.php
@@ -11,7 +11,21 @@ class CardController extends Controller
     {
         $path = $request->input('path');
         $type = $request->input('type') + 1;
-
-        return view('sc.card', compact('path', 'type'));
+        $country = '';
+        if ($type == 1) {
+            $country = '英国';
+        } elseif ($type == 2) {
+            $country = '土耳其';
+        } elseif ($type == 3) {
+            $country = '法国';
+        } elseif ($type == 4) {
+            $country = '马尔代夫';
+        } elseif ($type = 5) {
+            $country = '美国';
+        } elseif ($type = 6) {
+            $country = '希腊';
+        }
+        $js = \EasyWeChat::officialAccount();
+        return view('sc.card', compact('path', 'type', 'country', 'js'));
     }
 }
