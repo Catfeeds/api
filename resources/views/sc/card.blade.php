90de8493dd0a7f17d229bc8d3f09d16e3bf2d9d1
LOLO <lolosong@qq.com>
2018-11-23 16:20:02 +0800

    渣打

--- resources/views/sc/card.blade.php
+++ resources/views/sc/card.blade.php
@@ -77,7 +77,7 @@
       }
     })
     $('.audio2').on('ended', function () {
-      $(this).attr('src', pauseURL)
+      $(this).attr('src', playURL)
       $('.audio1')[0].pause()
       $('.audio2')[0].pause()
     })
