fcd8a83b7a24a13f5b6192bc10261ec5555aae81
zhaoyang017 <602155776@qq.com>
2018-12-07 13:20:57 +0800

    20181207 苏宁聚物节

--- resources/views/suning/shake.blade.php
+++ resources/views/suning/shake.blade.php
@@ -24,10 +24,10 @@
     <div class="foot"><img src="imgs/foot.png"></div>
 </section>
 </body>
-<script src="//api.shanghaichujie.com:3001/socket.io/socket.io.js"></script>
+<script src="http://api.touchworld-sh.com:3001/socket.io/socket.io.js"></script>
 
 <script>
-    var socket = io('api.shanghaichujie.com:3001');
+    var socket = io('http://api.touchworld-sh.com:3001');
     socket.emit('shake', '{"openid":"{{ $wechat['id'] }}"}');
     var shake = 4000,
         last_update = 0,
