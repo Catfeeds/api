526dcaccadf6996148604e178e8f610821dfb864
zhaoyang <602155776@qq.com>
2019-01-07 22:42:33 +0800

    天猫年货节 20180107

--- resources/views/tmall/nh.blade.php
+++ resources/views/tmall/nh.blade.php
@@ -8,7 +8,7 @@
   <!--http://www.html5rocks.com/en/mobile/mobifying/-->
   <meta name="viewport"
         content="width=device-width,user-scalable=no,initial-scale=1, minimum-scale=1,maximum-scale=1"/>
-  <meta name=”description” content="年货节品牌大街,等着你哟">
+  <meta name=”description” content="玩转年货节 宝盒寻不停">
 
   <!--https://developer.apple.com/library/safari/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html-->
   <meta name="apple-mobile-web-app-capable" content="yes">
@@ -106,7 +106,7 @@
 </head>
 <body>
   <audio id="audio" src="https://h5-touch.oss-cn-shanghai.aliyuncs.com/20190105_tmall/music.mp3" loop></audio>
-  <div style="display: none">年货节品牌大街,等着你哟</div>
+  <div style="display: none">玩转年货节 宝盒寻不停</div>
   <div id="splash">
     <div class="loading_box">
       <div class="progress-bar stripes">
@@ -168,7 +168,7 @@
         // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
         wx.onMenuShareAppMessage({
             title: '年货节品牌大街', // 分享标题
-            desc: "年货节品牌大街", // 分享描述
+            desc: "玩转年货节 宝盒寻不停", // 分享描述
             link: window.location.href,
             imgUrl: "https://h5-touch.oss-cn-shanghai.aliyuncs.com/logo/tmall_nh.png", // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
